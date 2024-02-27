a70447827e74e111f3331b750c84a1635d092a19
Elaidzha1940 <eli080293@gmail.com>
2024-02-27 16:50:00 +0300

    Added SecondScreen

--- UINavigationController/FirstScreen.swift
+++ UINavigationController/FirstScreen.swift
@@ -10,10 +10,41 @@
 import UIKit
 
 class FirstScreen: UIViewController {
+    
+    let nextButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray
+        view.backgroundColor = .systemGray4
+        setupButton()
+    }
+    
+    func setupButton() {
+        view.addSubview(nextButton)
+        
+        if #available(iOS 15.0, *) {
+            nextButton.configuration = .filled()
+        } else {
+            // Fallback on earlier versions
+        }
+        nextButton.configuration?.baseBackgroundColor = .systemPurple
+        nextButton.configuration?.title = "Next"
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -35),
+            nextButton.widthAnchor.constraint(equalToConstant: 300),
+            nextButton.heightAnchor.constraint(equalToConstant: 55)
+        
+        ])
     }
 }
+
+
+
+
+
+
 
---
+++ UINavigationController/SecondScreen.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Project: UINavigationController
+//  File: SecondScreen.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.02.2024
+//
+//  */
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray4
+    }
+}
